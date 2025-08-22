b4d58e0ec050220bc2876565570d7be84f987090
Mr.Invictus <arnabmandal261@gmail.com>
2025-08-22 22:05:16 +0530

    done

--- stack.c
+++ stack.c
@@ -1,54 +1,85 @@
-//implementation of stack using a#
+// Implementation of Stack using Array
 #include <stdio.h>
-#define size 5
-int top=-1;
-int stack[size];
-//push
-void push(int x)
-{
-if(top==size-1) printf("Stack overflow!\n");
-else
-    {
-        stack[++top]=x;
-        printf("%d pushed\n",x);
+#define SIZE 5
+
+int top = -1;
+int stack[SIZE];
+
+// Push
+void push(int x) {
+    if (top == SIZE - 1) 
+        printf("Stack overflow!\n");
+    else {
+        stack[++top] = x;
+        printf("%d pushed\n", x);
     }
 }
 
-//pop
-void pop()
-{
-    if (top== -1) printf("Stack underflow!\n");
-    else
-        {
-            printf("%d popped ",stack[top--]);
-        }
+// Pop
+void pop() {
+    if (top == -1) 
+        printf("Stack underflow!\n");
+    else 
+        printf("%d popped\n", stack[top--]);
 }
 
-//peek
-void peek()
-{
-    if(top ==-1)
+// Peek
+void peek() {
+    if (top == -1)
         printf("Stack is empty!\n");
     else
-        printf("Top element is %d\n",stack[top]);
+        printf("Top element is %d\n", stack[top]);
 }
 
-//display
-void display()
-{
-    if(top==-1) printf("The stack is empty!\n");
-    else
-        {
-            printf("Stack elements are!\n");
-            for(int i=top;i>=0;i--)
-            {
-                printf("%d",stack[i]);    
-            }
-            printf("\n");
+// Display
+void display() {
+    if (top == -1) 
+        printf("The stack is empty!\n");
+    else {
+        printf("Stack elements are:\n");
+        for (int i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
         }
+        printf("\n");
+    }
 }
 
-int main()
-{
+int main() {
+    int choice, n;
 
+    printf("Stack Implementation using Array\n");
+    do {
+        printf("\nEnter\n1. Push\n2. Pop\n3. Peek\n4. Display\n0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the element you want to push: ");
+                scanf("%d", &n);
+                push(n);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                peek();
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 0:
+                printf("Exiting program...\n");
+                break;
+
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 0);
+
+    return 0;
 }
