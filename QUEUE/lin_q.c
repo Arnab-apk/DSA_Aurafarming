3e654bd1ac73bea6e9b6809526c9323587a0ae58
Mr.Invictus <arnabmandal261@gmail.com>
2025-10-24 21:18:12 +0530

    d

---
+++ QUEUE/lin_q.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+# define size 5
+int q[5];
+int front=-1,rear=-1;
+
+int is_full()
+{
+  return rear==size-1; 
+}
+
+int is_empty()
+{
+  return (front==-1||front>rear);
+}
+
+void 
+
+
+
+
+
+
+int main()
+{
+
+}
